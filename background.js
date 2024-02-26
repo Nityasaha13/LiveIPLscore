1dfb555590343e2f548bc40553de1b22a816fbbe
Nityasaha13 <nityasaha39@gmail.com>
2024-02-27 00:10:57 +0530

    It works

--- background.js
+++ background.js
@@ -1,5 +1,10 @@
-let color = 'red';
-
 chrome.runtime.onInstalled.addListener(() => {
+    // Request eyedropper permission
+    chrome.permissions.request({
+        permissions: ['eyedropper'],
+    });
+
+    // Set initial color in storage
+    let color = 'red';
     chrome.storage.sync.set({ color });
 });
--- popup.js
+++ popup.js
@@ -3,30 +3,26 @@ const colorGrid = document.querySelector('.colorGrid');
 const colorValue = document.querySelector('.colorValue');
 
 btn.addEventListener('click', async () => {
-    chrome.storage.sync.get('color', ({ color }) => {
-        console.log('color: ', color);
-    });
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    chrome.scripting.executeScript(
-        {
-            target: { tabId: tab.id },
-            function: pickColor,
-        },
-        async (injectionResults) => {
-            const [data] = injectionResults;
-            if (data.result) {
-                const color = data.result.sRGBHex;
-                colorGrid.style.backgroundColor = color;
-                colorValue.innerText = color;
-                try {
-                    await navigator.clipboard.writeText(color);
-                } catch (err) {
-                    console.error(err);
-                }
+    if ('EyeDropper' in window) {
+        try {
+            const eyeDropper = new EyeDropper();
+            const result = await eyeDropper.open();
+            
+            const color = result.sRGBHex;
+            colorGrid.style.backgroundColor = color;
+            colorValue.innerText = color;
+            
+            try {
+                await navigator.clipboard.writeText(color);
+            } catch (err) {
+                console.error(err);
             }
+        } catch (err) {
+            console.error(err);
         }
-    );
+    } else {
+        console.error('EyeDropper API not supported in this browser.');
+    }
 });
 
 async function pickColor() {
