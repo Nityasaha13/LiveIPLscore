a620fb60f48f1e99c279d8c32412e8d2da88ec06
Nityasaha13 <nityasaha39@gmail.com>
2024-03-03 01:00:37 +0530

    Copy color code btn added

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 const btn = document.querySelector('.changeColorBtn');
 const colorGrid = document.querySelector('.colorGrid');
 const colorValue = document.querySelector('.colorValue');
+const copyButton = document.querySelector('.copyButton');
+const selectedColorDiv = document.querySelector('.selectedColor');
 
 btn.addEventListener('click', async () => {
     if ('EyeDropper' in window) {
@@ -11,6 +13,8 @@ btn.addEventListener('click', async () => {
             const color = result.sRGBHex;
             colorGrid.style.backgroundColor = color;
             colorValue.innerText = color;
+            
+            selectedColorDiv.style.display = 'flex';
             
             try {
                 await navigator.clipboard.writeText(color);
@@ -35,4 +39,20 @@ async function pickColor() {
     }
 }
 
+copyButton.addEventListener('click', () => {
+    const colorValueText = colorValue.innerText.trim();
+    
+    if (colorValueText !== '') {
+        navigator.clipboard.writeText(colorValueText)
+            .then(() => {
+                copyButton.innerText = 'Copied';
+                setTimeout(() => {
+                    copyButton.innerText = 'Copy';
+                }, 1000);
+            })
+            .catch((error) => {
+                console.error('Unable to copy color value to clipboard:', error);
+            });
+    }
+});
 
